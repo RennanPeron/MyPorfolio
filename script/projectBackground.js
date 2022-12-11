04a472c4f3cd5412b711b27dcee60a04f34bacbf
RennanPeron <rennanperon01@gmail.com>
2022-12-11 13:16:17 -0300

    fixing background

--- script/projectBackground.js
+++ script/projectBackground.js
@@ -3,12 +3,10 @@ function createBackground() {
     const projects = document.querySelectorAll('.projects')
     projects.forEach((item, key) => {
         if(item.firstElementChild.innerHTML == backgrounds[key]) {
-            item.style.background = `linear-gradient(rgba(224, 34, 34, 0) 0%, rgb(48, 47, 61) 70%) 0% 0% / cover, url("../img/${backgrounds[key]}.png") center top no-repeat`;
-            item.style.backgroundSize = "cover";
-            console.log(item.firstChild.style)
-            // item.style = `"background-size: cover; background: "linear-gradient(to bottom, rgba(224, 34, 34, 0) 0%, #302F3D  70%), url("../img/${backgrounds[key]}.png") no-repeat top center"`
-        }
+            item.style.background = `linear-gradient(to bottom, rgba(224, 34, 34, 0) 0%, #302F3D  70%), no-repeat center top / cover  url("../img/${backgrounds[key]}.png")`;
+        }   
     })
+
 }
 
 createBackground()
